6ce803e85aef20ce89d3aaf9fadb1ed5c0fbf711
MJK <myungjin_kim@tmax.co.kr>
2024-12-18 11:05:46 +0900

    feat: last commit time 출력 doc 용 사진 첨부

--- src/syncMonitor/view/ViewOracleTibero.java
+++ src/syncMonitor/view/ViewOracleTibero.java
@@ -5,6 +5,7 @@ import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
 import syncMonitor.query.oracle.OracleOnly;
 import syncMonitor.query.tibero.TiberoOnly;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -22,6 +23,8 @@ public class ViewOracleTibero implements View {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+            // Clean up the input string to remove .0 or any trailing fractional seconds
+
             // 제목 출력
             AsciiTable asciiTitle = new AsciiTable();
             asciiTitle.addRule();
@@ -32,14 +35,24 @@ public class ViewOracleTibero implements View {
             // 상태 테이블
             AsciiTable asciiTable = new AsciiTable();
             asciiTable.addRule();
-            asciiTable.addRow("TOPOLOGY", "Sync way", "SOURCE TSN", "TARGET TSN", "TSN_GAP");
+            asciiTable.addRow("TOPOLOGY", "SYNC WAY", "SOURCE TSN", "TARGET TSN", "TSN_GAP");
             asciiTable.addRule();
+            AsciiTable asciiTableTime= new AsciiTable();
+            asciiTableTime.addRule();
+            asciiTableTime.addRow("curr time" , LocalDateTime.now().format(formatter));
+            asciiTableTime.addRule();
 
             for (TopologyDto topology : topologyDtos) {
                 // DTO에서 객체 가져오기
+
                 TiberoOnly tiberoOnly = topology.getTiberoOnly();
                 OracleOnly oracleOnly = topology.getOracleOnly();
 
+                String tiberoTime = tiberoOnly.doGetTime().split("\\.")[0];
+                String oracleTime = oracleOnly.doGetTime().split("\\.")[0];
+
+                LocalDateTime tiberoLastCommitTime = LocalDateTime.parse(tiberoTime, formatter);
+                LocalDateTime oracleLastCommitTime = LocalDateTime.parse(oracleTime, formatter);
                 // 데이터 조회
                 Integer tiberoTsn = Integer.parseInt(tiberoOnly.doGetTsn());
                 Integer oraclePrsLct = Integer.parseInt(oracleOnly.doGetPrsLct());
@@ -51,12 +64,23 @@ public class ViewOracleTibero implements View {
                 asciiTable.addRule();
                 asciiTable.addRow(topology.getName(), "O->T", oracleSCN, tiberoPrsLct, (oracleSCN - tiberoPrsLct));
                 asciiTable.addRule();
+                asciiTable.addRule();
+
+                asciiTableTime.addRow("oracle last commit" , oracleLastCommitTime);
+                asciiTableTime.addRule();
+                asciiTableTime.addRow("tibero last commit" , tiberoLastCommitTime);
+                asciiTableTime.addRule();
+                asciiTableTime.addRule();
             }
 
             // 테이블 출력
+
+
             asciiTable.setTextAlignment(TextAlignment.CENTER);
             System.out.println(asciiTable.render(80));
 
+            asciiTableTime.setTextAlignment(TextAlignment.CENTER);
+            System.out.println(asciiTableTime.render(80));
         } catch (Exception e) {
             e.printStackTrace();
         }
