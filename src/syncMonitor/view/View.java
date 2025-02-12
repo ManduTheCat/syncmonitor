e63fa3b371d2f9c101fbb2c9b32f571dd4f8b79d
MJK <myungjin_kim@tmax.co.kr>
2025-02-12 17:58:21 +0900

    fix : 출력 UI 수정 마지막 반영시간 별도 컬럼으로 출력

--- src/syncMonitor/view/View.java
+++ src/syncMonitor/view/View.java
@@ -2,6 +2,7 @@ package syncMonitor.view;
 
 
 import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.asciitable.CWC_LongestLine;
 import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
 import lombok.Getter;
 import syncMonitor.config.wrapper.MonitorConfig;
@@ -15,47 +16,40 @@ public class View{
     @Getter
     private final List<Topology> topologyList;
 
-    private static Integer SIZE = 55;
+    private static Integer SIZE = 11;
 
     public View(List<Topology> topologyList , MonitorConfig monitorConfig) {
         this.topologyList = topologyList;
         if(monitorConfig.getMode().chars().allMatch(Character::isDigit)){
             SIZE = Integer.parseInt(monitorConfig.getMode());
         }else if ("WIDE".equalsIgnoreCase(monitorConfig.getMode())) {
-            SIZE = 70;
+            SIZE = 90;
         }
     }
     public void genView() {
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-            // Clean up the input string to remove .0 or any trailing fractional seconds
-
+            AsciiTable asciiTableTime= new AsciiTable();
+            asciiTableTime.addRule();
+            asciiTableTime.addRow("Prosync Monitor" , LocalDateTime.now().format(formatter));
+            asciiTableTime.addRule();
             // 제목 출력
             AsciiTable asciiTitle = new AsciiTable();
             asciiTitle.addRule();
-            asciiTitle.addRow("Prosync Monitor - Tibero & Oracle Sync Status").setTextAlignment(TextAlignment.CENTER);
+            asciiTitle.addRow("Prosync Monitor Sync Status").setTextAlignment(TextAlignment.CENTER);
             asciiTitle.addRule();
-            //System.out.println(asciiTitle.render(SIZE));
-
             AsciiTable asciiTable = new AsciiTable();
             asciiTable.addRule();
-            asciiTable.addRow("TOPOLOGY", "PSYC ID", "SOURCE TSN", "TARGET TSN", "TSN_GAP");
+            asciiTable.addRow("TOPOLOGY", "PSYC ID", "SOURCE TSN", "TARGET TSN", "TSN GAP",
+                    "Last Commit Time" );
             asciiTable.addRule();
-            AsciiTable asciiTableTime= new AsciiTable();
-            asciiTableTime.addRule();
-            asciiTableTime.addRow("curr time" , LocalDateTime.now().format(formatter));
-            asciiTableTime.addRule();
-            for (Topology topology : topologyList) {
-                // DTO에서 객체 가져오기
-                //String LastCommitTimeRes = topology.runTargetTimeQuery().split("\\.")[0];
-                //String LastCommitTimeRes = topology.runTargetTimeQuery();
 
-                //LocalDateTime targetCommitTime = LocalDateTime.parse(LastCommitTimeRes, formatter);
-                String targetCommitTime = null;
+
+            for (Topology topology : topologyList) {
+                String targetCommitTime = "null";
                 try{
                     targetCommitTime = topology.runTargetTimeQuery();
-                    //System.out.println(LocalDateTime.parse(targetCommitTime, formatter));
                 }catch (Exception e){
                     targetCommitTime = "null";
                 }
@@ -63,8 +57,20 @@ public class View{
                 try{
                     Integer sourceCn = topology.runSourceChangeNumberQuery();
                     Integer targetCn = topology.runTargetChangeNumberQuery();
+                    String sourceRes = String.valueOf(sourceCn);
+                    String targetRes = String.valueOf(targetCn);
+                    String diffRes = String.valueOf(sourceCn - targetCn);
+                    if(sourceCn== -1){
+                        sourceRes = "source fail";
+                    }
+                    if(targetCn== -1){
+                        targetRes = "target fail";
+                    }
+                    if(sourceCn == -1 || targetCn == -1){
+                        diffRes = "fail";
+                    }
                     asciiTable.addRow(topology.getTopologyName() == null ? "null" : topology.getTopologyName(),
-                            topology.getProSyncUser(), sourceCn, targetCn, (sourceCn - targetCn));
+                            topology.getProSyncUser(), sourceRes, targetRes, diffRes, targetCommitTime);
                     asciiTable.addRule();
                 }catch (Exception e){
                     String sourceCn = topology.getTopologyName()+" src fail";
@@ -74,27 +80,21 @@ public class View{
                     asciiTable.addRule();
                 }
 
-//                System.out.println("sourceCn: " +sourceCn);
-//                System.out.println("targetCn:" + targetCn);
-                // 테이블 행 추가
+//                asciiTableTime.addRow(topology.getTopologyName()+" commit to target at" , targetCommitTime);
+//                asciiTableTime.addRule();
 
-                asciiTableTime.addRow(topology.getTopologyName()+" commit to target at" , targetCommitTime);
-                asciiTableTime.addRule();
-
-                //topology.getTarget().getSession().disconnect();
-                //topology.getSource().getSession().disconnect();
             }
 
             // 테이블 출력
-
-
+            CWC_LongestLine cwc = new CWC_LongestLine();
             asciiTable.setTextAlignment(TextAlignment.CENTER);
-            System.out.println(asciiTable.render(SIZE));
+            cwc.add(SIZE, 0).add(SIZE,0).add(SIZE,0).add(SIZE,0).add(SIZE,0);
+            asciiTable.getRenderer().setCWC(cwc);
+//            asciiTable.getContext().setWidth(SIZE);
+            System.out.println(asciiTable.render());
 
-            asciiTableTime.setTextAlignment(TextAlignment.CENTER);
-            System.out.println(asciiTableTime.render(SIZE));
-
-
+//            asciiTableTime.setTextAlignment(TextAlignment.CENTER);
+//            System.out.println(asciiTableTime.render(SIZE));
 
         } catch (Exception e) {
             e.printStackTrace();
